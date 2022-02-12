6552a965485fcd80aedf3075b983576044466877
Filip Åhfelt <filip.ahfelt@devoteam.com>
2022-02-12 22:00:08 +0100

    feat: usespotify hook

---
+++ src/hooks/useSpotify.ts
@@ -0,0 +1,21 @@
+import { signIn, useSession } from 'next-auth/react'
+import { useEffect } from 'react'
+import SpotifyWebApi from 'spotify-web-api-node'
+
+const spotifyApi = new SpotifyWebApi()
+
+const useSpotify = () => {
+  const { data: session } = useSession()
+
+  useEffect(() => {
+    if (session) {
+      session.error === 'RefreshAccessTokenError' && signIn() // Force sign in
+      // @ts-ignore
+      spotifyApi.setAccessToken(session.user.accessToken)
+    }
+  }, [session])
+
+  return spotifyApi
+}
+
+export default useSpotify
