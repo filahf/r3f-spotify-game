58abc90172f66fa00bfefa526bee6cb714673299
Filip Åhfelt <filip.ahfelt@devoteam.com>
2022-02-13 14:58:41 +0100

    fix: box types

--- src/components/canvas/Box.tsx
+++ src/components/canvas/Box.tsx
@@ -1,17 +1,22 @@
 import useStore from '@/helpers/store'
 import { useFrame } from '@react-three/fiber'
 import { useRef, useState } from 'react'
+import * as THREE from 'three'
 
-const BoxComponent = ({ route }) => {
+type BoxComponentProps = {
+  route?: string
+}
+
+const BoxComponent = ({ route }: BoxComponentProps) => {
   const router = useStore((s) => s.router)
   // This reference will give us direct access to the THREE.Mesh object
-  const mesh = useRef(null)
+  const mesh = useRef<THREE.Mesh>(null)
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) =>
     mesh.current
-      ? (mesh.current.rotation.y = mesh.current.rotation.x += 0.01)
+      ? (mesh.current!.rotation.y = mesh.current!.rotation.x += 0.01)
       : null
   )
   // Return the view, these are regular Threejs elements expressed in JSX
@@ -19,13 +24,13 @@ const BoxComponent = ({ route }) => {
     <>
       <mesh
         ref={mesh}
-        onClick={() => router.push(route)}
+        onClick={route ? () => router && router.push(route) : undefined}
         onPointerOver={() => setHover(true)}
         onPointerOut={() => setHover(false)}
         scale={hovered ? 1.1 : 1}
       >
         <boxBufferGeometry args={[1, 1, 1]} />
-        <meshPhysicalMaterial color={route === '/' ? 'orange' : 'hotpink'} />
+        <meshPhysicalMaterial color={'#1DB954'} />
       </mesh>
       <directionalLight position={[5, 5, 5]} />
       <ambientLight />
