5d9b651773565af30e7a2a7f320d6542af986399
Filip Åhfelt <filip.ahfelt@devoteam.com>
2022-02-13 11:56:12 +0100

    chore: clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Infobox from '@/components/dom/Infobox'
-
-import { GetServerSideProps } from 'next'
-import { getSession, useSession } from 'next-auth/react'
 import dynamic from 'next/dynamic'
 
 const Box = dynamic(() => import('@/components/canvas/Box'), {
@@ -35,13 +32,3 @@ const Page = () => {
 }
 
 export default Page
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = await getSession(ctx)
-  return {
-    props: {
-      title: 'Box',
-      session: session,
-    },
-  }
-}
