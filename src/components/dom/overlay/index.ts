ca47ce18707e3672c69ffb906394999205b35395
Filip Åhfelt <filip.ahfelt@devoteam.com>
2022-03-08 12:54:36 +0100

    refactor: landing dom component

--- src/components/dom/overlay/index.ts
+++ src/components/dom/overlay/index.ts
@@ -1,2 +1,3 @@
 export { default as Onboarding } from './Onboarding'
 export { default as InGame } from './InGame'
+export { default as Landing } from './Landing'
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,44 +1,9 @@
-import { Button, Heading, Icon, Stack, Text } from '@chakra-ui/react'
-import { signIn } from 'next-auth/react'
-import { BsSpotify } from 'react-icons/bs'
+import { Landing } from '@/components/dom/overlay'
 
 const Login = () => {
   return (
     <>
-      <Stack
-        textAlign={'center'}
-        align={'center'}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}
-      >
-        <Heading
-          fontWeight={600}
-          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-          lineHeight={'110%'}
-        >
-          Lorem Ipsum{' '}
-          <Text as={'span'} color={'green.400'}>
-            made easy
-          </Text>
-        </Heading>
-        <Text color={'gray.500'} maxW={'3xl'}>
-          This game is built using Next.js + Three.js. To enter the game you
-          need to be authorized.
-        </Text>
-        <Stack spacing={6} direction={'row'}>
-          <Button
-            rounded={'full'}
-            px={6}
-            colorScheme={'green'}
-            bg={'green.400'}
-            _hover={{ bg: 'green.500' }}
-            onClick={() => signIn('spotify', { callbackUrl: '/' })}
-            rightIcon={<Icon as={BsSpotify} mb={1} />}
-          >
-            Sign in with Spotify
-          </Button>
-        </Stack>
-      </Stack>
+      <Landing />
     </>
   )
 }
