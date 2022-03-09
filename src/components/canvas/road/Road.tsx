d0354abd34a6c27f1255e6509e972a966c074854
Filip Åhfelt <filip.ahfelt@devoteam.com>
2022-03-09 14:03:29 +0100

    fix: update camera movement

--- src/components/canvas/road/Road.tsx
+++ src/components/canvas/road/Road.tsx
@@ -1,8 +1,7 @@
 import { ROAD_LENGTH } from '@/shared/constants'
-import { getXDistortion, getYDistortion } from '@/utils/distortion'
-import { PerspectiveCamera, Plane } from '@react-three/drei'
+import { Plane } from '@react-three/drei'
 import { extend, useFrame } from '@react-three/fiber'
-import { useMemo, useRef } from 'react'
+import { useRef } from 'react'
 import * as THREE from 'three'
 
 import RoadShaderMaterial, { RoadShaderMaterialImpl } from './shader'
@@ -12,51 +11,16 @@ extend({ RoadShaderMaterial })
 const Road = () => {
   const ref = useRef<RoadShaderMaterialImpl>()
 
-  const cameraRef = useRef<THREE.Camera>()
-
-  // https://docs.pmnd.rs/react-three-fiber/advanced/pitfalls#tips-and-tricks
-  const lookAtAmp = useMemo(() => new THREE.Vector3(1, 1, 0), [])
-  const lookAtOffset = useMemo(() => new THREE.Vector3(0, 0, -5), [])
-
-  const lookAtTemp = useMemo(() => new THREE.Vector3(0, 0, 0), [])
-  const lookAt = useMemo(() => new THREE.Vector3(), [])
-
   useFrame(({ clock }) => {
-    if (ref.current && cameraRef.current) {
-      const progress = 0.025
+    if (ref.current) {
       const time = clock.getElapsedTime()
 
       ref.current.uTime = time
-      lookAtTemp.x = getXDistortion(progress, time)
-
-      lookAtTemp.y = getYDistortion(progress, time)
-
-      lookAt.x =
-        cameraRef.current.position.x +
-        lookAtTemp.multiply(lookAtAmp).add(lookAtOffset).x
-      lookAt.y =
-        cameraRef.current.position.y +
-        lookAtTemp.multiply(lookAtAmp).add(lookAtOffset).y
-      lookAt.z =
-        cameraRef.current.position.z +
-        lookAtTemp.multiply(lookAtAmp).add(lookAtOffset).z
-
-      cameraRef.current.lookAt(lookAt)
     }
   })
 
   return (
     <>
-      <pointLight position={[10, 10, 10]} />
-      <ambientLight />
-      <PerspectiveCamera
-        fov={100}
-        near={0.5}
-        far={ROAD_LENGTH}
-        ref={cameraRef}
-        position={[0, 8, -5]}
-        makeDefault
-      />
       <Plane
         args={[30, ROAD_LENGTH, 20, 100]}
         rotation={[-Math.PI / 2, 0, 0]}
--- src/views/canvas/GameCanvas.tsx
+++ src/views/canvas/GameCanvas.tsx
@@ -4,14 +4,44 @@ import Explosions from '@/components/canvas/explosion/Explosion'
 import { Road } from '@/components/canvas/road'
 import { GameShip } from '@/components/canvas/ship'
 import { Targets } from '@/components/canvas/targets'
+import { ROAD_LENGTH } from '@/shared/constants'
 import useStore from '@/shared/store'
-import { Stars } from '@react-three/drei'
-import { Suspense } from 'react'
+import { getYDistortion } from '@/utils/distortion'
+import { CameraShake, PerspectiveCamera, Stars } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { Suspense, useMemo } from 'react'
+import * as THREE from 'three'
 
 const GameCanvas = () => {
   const selectedTrack = useStore((s) => s.selectedTrack)
+  const ship = useStore((s) => s.ship)
+
+  const lookAt = useMemo(() => new THREE.Vector3(), [])
+
+  useFrame(({ camera, clock }) => {
+    if (ship.current) {
+      const time = clock.getElapsedTime()
+      const progress = 0.025
+      lookAt.set(ship.current.position.x, ship.current.position.y, -50)
+
+      camera.position.x = ship.current.position.x
+      camera.position.y = getYDistortion(progress, time) + 8
+
+      camera.lookAt(lookAt)
+    }
+  })
+
   return (
     <>
+      <pointLight position={[10, 10, 10]} />
+      <ambientLight />
+      <PerspectiveCamera
+        fov={100}
+        near={0.5}
+        far={ROAD_LENGTH}
+        position={[0, 5, -5]}
+        makeDefault
+      />
       <GameShip />
       <Road />
       <Targets />
@@ -23,6 +53,7 @@ const GameCanvas = () => {
         )}
       </Suspense>
       <Stars radius={120} count={600} />
+      <CameraShake maxYaw={0} maxPitch={0} maxRoll={0.05} />
       <Effects />
     </>
   )
