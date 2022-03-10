909b826cca5bc57046b21f60eb36ee5d00a478cf
Filip Åhfelt <filip.ahfelt@devoteam.com>
2022-03-10 09:50:03 +0100

    feat: add ssao

--- src/components/canvas/effects/Effects.tsx
+++ src/components/canvas/effects/Effects.tsx
@@ -2,6 +2,7 @@ import {
   Bloom,
   EffectComposer,
   Noise,
+  SSAO,
   Vignette,
 } from '@react-three/postprocessing'
 
@@ -10,12 +11,13 @@ const Effects = () => {
     <EffectComposer multisampling={8}>
       <Bloom
         kernelSize={4}
-        luminanceThreshold={0.3}
+        luminanceThreshold={0.2}
         luminanceSmoothing={0.3}
         intensity={0.2}
       />
       <Noise opacity={0.02} />
       <Vignette eskil={false} offset={0.1} darkness={1.1} />
+      <SSAO />
     </EffectComposer>
   )
 }
